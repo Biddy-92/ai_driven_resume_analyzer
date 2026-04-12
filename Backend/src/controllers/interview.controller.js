a91e92e5e3e78a474df3e9c880726eeff83db0c4
Bidhan CKB <bidhanckb1000@gmail.com>
2026-04-12 15:02:48 +0600

    Remove all files listed in .gitignore from Git tracking

--- Backend/src/controllers/interview.controller.js
+++
@@ -1,4 +0,0 @@
-
-const pdfParse = require("pdf-parse")
-const {generateInterviewReport, generateResumePdf } = require("../services/ai.service")
-const interviewReportModel = require("../models/interviewReport.model")
