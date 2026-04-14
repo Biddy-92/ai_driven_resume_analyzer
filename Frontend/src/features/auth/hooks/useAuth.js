d182263e42b167b55284bde348a6640659300ef4
Bidhan CKB <bidhanckb1000@gmail.com>
2026-04-14 20:03:25 +0600

    useAuth.js hook complete

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,69 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../auth.context";
+import { login, register, logout, getMe } from "../services/auth.api";
+
+
+
+export const useAuth = () => {
+
+    const context = useContext(AuthContext)
+    const { user, setUser, loading, setLoading } = context
+
+
+    const handleLogin = async ({ email, password }) => {
+        setLoading(true)
+        try {
+            const data = await login({ email, password })
+            setUser(data.user)
+        } catch (err) {
+            console.error("Login error:", err)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const handleRegister = async ({ username, email, password }) => {
+        setLoading(true)
+        try {
+            const data = await register({ username, email, password })
+            setUser(data.user)
+        } catch (err) {
+            console.error("Register error:", err)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const handleLogout = async () => {
+        setLoading(true)
+        try {
+            const data = await logout()
+            setUser(null)
+            console.log(data.message);  // "User logged out successfully"
+        } catch (err) {
+            console.error("Logout error:", err)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+
+        const getAndSetUser = async () => {
+            try {
+
+                const data = await getMe()
+                setUser(data.user)
+            } catch (err) {
+                console.error("GetMe error:", err)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        getAndSetUser()
+
+    }, [])
+
+    return { user, loading, handleRegister, handleLogin, handleLogout }
+}
